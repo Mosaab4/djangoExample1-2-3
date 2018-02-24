2d773cfe31b4e8ad54e84246c645eadc0c313212
mosaab <musab.ehab@gmail.com>
2018-02-24 16:11:24 +0200

    Add similar posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.core.mail import send_mail
+from django.db.models import Count
+
 from taggit.models import Tag
+
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
@@ -61,6 +64,9 @@ def post_detail(request, year,month,day,post):
         comment_form = CommentForm()
         new_comment = False
 
+    post_tags_ids = post.tags.values_list('id',flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
     return render(
         request,
         'blog/post/detail.html',
@@ -69,6 +75,7 @@ def post_detail(request, year,month,day,post):
             'comments': comments,
             'comment_form': comment_form,
             'new_comment': new_comment,
+            'similar_posts':similar_posts,
         }
     )
 
